d2a26353585e65242ebb51a03d1096643a551b42
LukeCarlson <42789571+Skylake101@users.noreply.github.com>
2021-04-01 08:35:16 -0500

    Update Basic Password Cracker.py

--- Basic Password Cracker.py
+++ Basic Password Cracker.py
@@ -1,5 +1,5 @@
 #This program can crack a password by attemtping every possible combination of letters
-#It would never work in the real world since real login pages have limited attempts and often captchas
+#It obviously doesn't work in real login pages since they have limited attempts and often captchas
 
 import random
 print("****************************************\n*** 		Breaking the Code	  ***\n****************************************\n")
